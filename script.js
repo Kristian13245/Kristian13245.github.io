b1b34cdec2c5ef472aecb8c266e5b6de4b384dde
Kristian13245 <kristian.kraljevic@hotmail.com>
2023-07-15 14:03:25 +0200

    first version done

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+/*
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    document.getElementsByClassName("navigation")[0].style.backgroundColor = "red";
+  } else {
+    document.getElementsByClassName("navigation")[0].style.backgroundColor = "transparent";
+  }
+}
+*/
