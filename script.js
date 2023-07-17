a34939a09b567edf25a429fd3ea1630db4689e41
Kristian13245 <kristian.kraljevic@hotmail.com>
2023-07-17 19:52:37 +0200

    responsive header

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+const hamburger = document.querySelector(".hamburger");
+const headerLinks = document.querySelector(".header_links");
 
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  headerLinks.classList.toggle("active");
+})
 
-/*
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    document.getElementsByClassName("navigation")[0].style.backgroundColor = "red";
-  } else {
-    document.getElementsByClassName("navigation")[0].style.backgroundColor = "transparent";
-  }
-}
-*/
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  headerLinks.classList.remove("active");
+}))
