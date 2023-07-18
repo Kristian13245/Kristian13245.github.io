74796702690a8e7984e37b2fc967aee4f6e4cd3e
Kristian13245 <kristian.kraljevic@hotmail.com>
2023-07-18 12:41:08 +0200

    animationen und seo

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*Hamburger Menu*/
 const hamburger = document.querySelector(".hamburger");
 const headerLinks = document.querySelector(".header_links");
 
@@ -10,3 +11,21 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   headerLinks.classList.remove("active");
 }))
+
+/*Reveal Animation*/
+function show() {
+  var reveal = document.querySelectorAll(".reveal");
+
+  for (var i = 1; i < reveal.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveal[i].getBoundingClientRect().top;
+    var e = 160;
+
+    if (elementTop < windowHeight-e) {
+      reveal[i].classList.add("active");}
+    else {;
+    }
+  }
+}
+
+window.addEventListener("scroll", show);
